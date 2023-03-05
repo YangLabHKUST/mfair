9e51fcfd2f08d329de55e199213cea0bbc039e5e
WANG Zhiwei <48282751+statwangz@users.noreply.github.com>
2023-03-05 17:32:31 +0800

    Add predict function

---
+++ R/mfairPredict.R
@@ -0,0 +1,28 @@
+#' Predict function for MFAIR object.
+#'
+#' @param MFAIR MFAIR object.
+#'
+#' @return Predicted matrix with the same dimension as that of Y.
+#' @export
+#'
+#' @examples
+#' MFAIR <- createMFAIR(Y, X)
+#' MFAIR <- fitGreedy(MFAIR)
+#' Y_hat <- predict(MFAIR)
+setMethod(
+  f = "predict",
+  signature = "MFAIR",
+  definition = function(object) {
+    # Check Y
+    if (object@Y_missing == FALSE) {
+      stop("The main data matrix Y has no missing entries!")
+    } # End
+
+    # Check inferred loading Z and factor W
+    if (length(object@Z) == 0 && length(object@W) == 0) {
+      stop("The model has not beed fitted!")
+    } # End
+
+    return(Y_hat = object@Z %*% t(object@W))
+  }
+)
