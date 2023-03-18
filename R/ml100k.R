ab80900dbab7240c566ef63ebbe987b4f3e9d799
WANG Zhiwei <48282751+statwangz@users.noreply.github.com>
2023-03-19 01:23:41 +0800

    Add MovieLens 100K data

---
+++ R/ml100k.R
@@ -0,0 +1,13 @@
+#' MovieLens 100K dataset.
+#'
+#' A list containing the movie rating, user information, and movie genres of the MovieLens 100K dataset.
+#'
+#' @format A list containing a movie rating matrix, a user information data frame, and a movie genres data frame.
+#' \describe{
+#'   \item{rating}{Movie rating matrix of 1,682 movies and 943 users (0–5 star rating).}
+#'   \item{user}{A data frame containing the information of 943 users, the three columns correspond to age, gender, and occupation respectively.}
+#'   \item{genre}{A binary data frame containing the genre information of 1,682 movies, each column corresponds to one specific genre.}
+#' }
+#' @source \url{https://grouplens.org/datasets/movielens/100k/}
+#' @name ml100k
+NULL
---
+++ data-raw/ml100k.R
@@ -0,0 +1,48 @@
+### code to prepare `ml100k` dataset goes here
+
+## Movie rating data
+u_data <- read.table(system.file("extdata/MovieLens/ml-100k", "u.data", package = "mfair"),
+  sep = "\t", header = FALSE
+)
+# head(u_data)
+# dim(u_data)
+
+# range(u_data[, 1])
+N <- (range(u_data[, 1]))[2]
+# range(u_data[, 2])
+M <- (range(u_data[, 2]))[2]
+
+u_data_matrix <- matrix(NA, N, M)
+for (i in 1:dim(u_data)[1]) {
+  u_data_matrix[u_data[i, 1], u_data[i, 2]] <- u_data[i, 3]
+}
+# head(u_data_matrix)
+
+## User information
+u_user <- read.table(system.file("extdata/MovieLens/ml-100k", "u.user", package = "mfair"),
+  sep = "|", header = FALSE
+)
+# user id | age | gender | occupation | zip code
+# head(u_user)
+# dim(u_user)
+
+u_user <- u_user[, -c(1, 5)]
+u_user[, 2] <- factor(u_user[, 2])
+u_user[, 3] <- factor(u_user[, 3])
+colnames(u_user) <- c("Age", "Gender", "Occupation")
+# head(u_user)
+
+# Movie genre information
+u_item <- read.csv(system.file("extdata/MovieLens/ml-100k", "u.item", package = "mfair"),
+  sep = "|", header = FALSE
+)
+# head(u_item)
+# dim(u_item)
+
+u_item <- u_item[, c(6:24)]
+colnames(u_item) <- c("Unknown", "Action", "Adventure", "Animation", "Children‘s", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western")
+
+ml100k <- list(rating = u_data_matrix, user = u_user, genre = u_item)
+
+# Save the data
+usethis::use_data(ml100k, overwrite = TRUE)
