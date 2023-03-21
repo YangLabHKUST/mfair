9f95618f4bb92a6e5921c1046148ca3608bab5f6
WANG Zhiwei <48282751+statwangz@users.noreply.github.com>
2023-03-21 22:44:32 +0800

    Update ml100k.R

--- data-raw/ml100k.R
+++ data-raw/ml100k.R
@@ -4,9 +4,6 @@
 u_data <- read.table("https://files.grouplens.org/datasets/movielens/ml-100k/u.data",
   sep = "\t", header = FALSE
 )
-# u_data <- read.table(system.file("extdata/ml-100k", "u.data", package = "mfair"),
-#   sep = "\t", header = FALSE
-# )
 # head(u_data)
 # dim(u_data)
 
@@ -25,9 +22,6 @@ for (i in 1:dim(u_data)[1]) {
 u_user <- read.table("https://files.grouplens.org/datasets/movielens/ml-100k/u.user",
   sep = "|", header = FALSE
 )
-# u_user <- read.table(system.file("extdata/ml-100k", "u.user", package = "mfair"),
-#   sep = "|", header = FALSE
-# )
 # user id | age | gender | occupation | zip code
 # head(u_user)
 # dim(u_user)
@@ -37,19 +31,24 @@ u_user[, 2] <- factor(u_user[, 2])
 u_user[, 3] <- factor(u_user[, 3])
 colnames(u_user) <- c("Age", "Gender", "Occupation")
 # head(u_user)
+# dim(u_user)
 
 # Movie genre information
 u_item <- read.csv("https://files.grouplens.org/datasets/movielens/ml-100k/u.item",
   sep = "|", header = FALSE
 )
-# u_item <- read.csv(system.file("extdata/ml-100k", "u.item", package = "mfair"),
-#   sep = "|", header = FALSE
-# )
 # head(u_item)
 # dim(u_item)
 
+# all_genres <- read.csv("https://files.grouplens.org/datasets/movielens/ml-100k/u.genre",
+#   sep = "|", header = FALSE
+# )
+# all_genres
+
 u_item <- u_item[, c(6:24)]
 colnames(u_item) <- c("Unknown", "Action", "Adventure", "Animation", "Children‘s", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western")
+# head(u_item)
+# dim(u_item)
 
 ml100k <- list(rating = u_data_matrix, user = u_user, genre = u_item)
 
