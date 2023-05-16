54b44b594b3d364ea97f36ae41f00da3fdf79b53
WANG Zhiwei <48282751+statwangz@users.noreply.github.com>
2023-05-16 20:29:23 +0800

    Update the description of the neocortex data

--- R/neocortexData.R
+++ R/neocortexData.R
@@ -2,10 +2,10 @@
 #'
 #' A list containing the bulk gene expression of human brain and tissue sample information.
 #'
-#' @format A list containing a gene expression matrix and a tissue sample information data frame.
+#' @format A list containing a gene expression data matrix and a tissue sample information data frame.
 #' \describe{
-#'   \item{expression}{Bulk gene expression matrix of 886 tissue samples and 2,000 genes with highest differential stability.}
-#'   \item{sample_info}{A data frame containing the information of 886 bulk tissue samples, the four columns correspond to sample ID, neocortex area, hemisphere, and time periods respectively.}
+#'   \item{expression}{Bulk gene expression matrix of 886 tissue samples in the neocortex region and 2,000 genes with highest differential stability.}
+#'   \item{sample_info}{A data frame containing the information of 886 bulk tissue samples in the neocortex region. The four columns correspond to sample ID, neocortex area, hemisphere, and time periods respectively.}
 #' }
 #' @source \url{https://hbatlas.org/pages/data}
 #' @name neocortex
