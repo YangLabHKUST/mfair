2decb0c456aaba8923544ddbaaa29918c5bb1fff
WANG Zhiwei <48282751+statwangz@users.noreply.github.com>
2023-10-02 22:23:08 +0800

    Don't export the utils function

--- R/mfairUtils.R
+++ R/mfairUtils.R
@@ -6,7 +6,6 @@
 #' @param obs_indices A matrix containing 1-based indices of the observed entries in the matrix Y. The first column represents row and the second column represents column.
 #'
 #' @return A dgCMatrix containing the projection result.
-#' @export
 #'
 projSparse <- function(Y, obs_indices) {
   N <- nrow(Y)
